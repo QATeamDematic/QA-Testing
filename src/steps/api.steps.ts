6001f7f66a61747f9cc321c48f6d0f0ac571918d
Saheli Sen <A0085151@kiongroup.com>
2023-06-15 10:25:38 -0400

    comment added

--- src/steps/api.steps.ts
+++ src/steps/api.steps.ts
@@ -1,7 +1,7 @@
 import { ICustomWorld } from '../support/custom-world';
 import { expect } from '@playwright/test';
 import { Given } from '@cucumber/cucumber';
-
+// the fact for testing
 Given('A cat fact is recieved', async function (this: ICustomWorld) {
   const response = await this.server?.get('facts');
   expect(response).toBeDefined();
